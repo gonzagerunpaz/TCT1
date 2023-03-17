7adbec797d1b0621a7d5208b7f60590fae3dee7b
Gonzalez <a446857@lbr000258.rio.ar.bsch>
2023-03-16 21:09:46 -0300

    tercera confirmacion

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('Hola Mundo!!!, desde la UNPAZ');
+import fetch  from 'node-fetch';
+
+fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then(data => {
+    const filtrado = data.products.filter( p => p.price >= 1000 )
+    const transformado = filtrado.map( p => {   return { nombre: p.title, precio: p.price, stock: p.stock, valuacion: p.price * p.stock } })
+    console.log(transformado);
+    
+    let acum = 0
+    transformado.forEach(element => {
+      acum += element.valuacion
+    });
+
+    console.log(acum);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
